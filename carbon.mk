3cec5fa4a5df8cb35bdb0f02ee84a882c8f2189d
MURA Stefano [STELIX PippoX3] <ssspinni@gmail.com>
2014-08-11 17:37:41 +0200

    Revert "bacon: Update model name to One A0001"
    This reverts commit 74be6cc65159bb4d74e5f2cb2a1a2aea2a35f133.
    
    Signed-off-by: MURA Stefano [STELIX PippoX3] <STELIX@gmail.com>

--- carbon.mk
+++ carbon.mk
@@ -1,8 +1,17 @@
-## Specify phone tech before including full_phone
-#$(call inherit-product, vendor/slim/config/gsm.mk)
-
-# Release name
-PRODUCT_RELEASE_NAME := bacon
+# Copyright (C) 2014 The CyanogenMod Project
+# Copyright (C) 2014 The Carbon Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1920
@@ -11,24 +20,29 @@ TARGET_SCREEN_WIDTH := 1080
 # languages
 PRODUCT_LOCALES := en_US de_DE zh_CN zh_TW cs_CZ nl_BE nl_NL en_AU en_GB en_CA en_NZ en_SG fr_BE fr_CA fr_FR fr_CH de_AT de_LI de_CH it_IT it_CH ja_JP ko_KR pl_PL ru_RU es_ES ar_EG ar_IL bg_BG ca_ES hr_HR da_DK en_IN en_IE en_ZA fi_FI el_GR iw_IL hi_IN hu_HU in_ID lv_LV lt_LT nb_NO pt_BR pt_PT ro_RO sr_RS sk_SK sl_SI es_US sv_SE tl_PH th_TH tr_TR uk_UA vi_VN
 
-# Inherit some common Slim stuff.
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from bacon device
+$(call inherit-product, device/oneplus/bacon/bacon.mk)
+
+# Inherit some common Carbon stuff.
 $(call inherit-product, vendor/carbon/config/common_phone.mk)
 
-# Enhanced NFC
-#$(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
-
-# Inherit device configuration
-$(call inherit-product, device/oneplus/bacon/full_bacon.mk)
-
-## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := bacon
 PRODUCT_NAME := carbon_bacon
-PRODUCT_BRAND := oneplus
-PRODUCT_MODEL := A0001
+PRODUCT_DEVICE := bacon
 PRODUCT_MANUFACTURER := OnePlus
+PRODUCT_MODEL := One
 PRODUCT_PROPERTY_OVERRIDES += ro.buildzipid=carbon.bacon.$(shell date +%m%d%y).$(shell date +%H%M%S)
-CARBON_BUILDTYPE := ALPHA
+CARBON_BUILDTYPE := BETA
+
+PRODUCT_GMS_CLIENTID_BASE := android-oneplus
+
+PRODUCT_BRAND := oneplus
+TARGET_VENDOR_PRODUCT_NAME := bacon
+TARGET_VENDOR_DEVICE_NAME := A0001
+PRODUCT_BUILD_PROP_OVERRIDES += TARGET_DEVICE=A0001 PRODUCT_NAME=bacon
 
 TARGET_CONTINUOUS_SPLASH_ENABLED := true
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=carbon_bacon BUILD_FINGERPRINT="fingerprint: oneplus/bacon/A0001:4.4.2/KVT49L/XNPH22R:user/release-keys" PRIVATE_BUILD_DESC="bacon-user 4.4.2 KVT49L XNPH22R release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=oneplus/bacon/A0001:4.4.2/KVT49L/XNPH25R:user/release-keys PRIVATE_BUILD_DESC="bacon-user 4.4.2 KVT49L XNPH25R release-keys"
